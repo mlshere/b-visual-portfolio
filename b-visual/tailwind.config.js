b4126e5a158527992b7c5f195f92624fe4ff8bc9
mlshere <mlsherezade@gmail.com>
2025-04-26 09:00:37 +0200

    Add theme and styling

--- b-visual/tailwind.config.js
+++ b-visual/tailwind.config.js
@@ -1,15 +1,32 @@
+import tailwindScrollbar from 'tailwind-scrollbar';
+import tailwindScrollbarHide from 'tailwind-scrollbar-hide';
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        ".index.html",
+        "./index.html",
         "./src/**/*.{html,js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                background: "#000000",
+                text: "#FFFFFF",
+                accent: "#c59c6c"
+            },
+            fontFamily: {
+                sans: ["Helvetica Neue", ...defaultTheme.fontFamily.sans],
+                display: ["Anton", "Impact", "sans-serif"],
+            },
+            fontSize: {
+                'menu': '2rem'
+            }
+        },
     },
     plugins: [
-        require('tailwind-scrollbar'),
-        require('tailwind-scrollbar-hide'),
-        require('tailwindcss-animate'),
+        tailwind-scrollbar,
+        tailwind-scrollbar-hide,
+        tailwindcss-animate,
     ],
 }
