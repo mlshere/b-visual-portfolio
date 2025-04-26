36966a8d193e227fa17e2c937696ace14b52ba53
mlshere <mlsherezade@gmail.com>
2025-04-26 09:02:15 +0200

    Main layout

--- b-visual/src/App.jsx
+++ b-visual/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from "./components/Navbar";
+import MenuOverlay from "./components/MenuOverlay";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+    <div className="relative w-full h-full">
+      <Navbar toggleMenu={() => setMenuOpen(!menuOpen)} />
+      {menuOpen && <MenuOverlay closeMenu={() => setMenuOpen(false)} />}
+
+      {/* <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/reel" element={<Reel />} />
+        <Route path="/works" element={<Works />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/" element={<Home />} />
+      </Routes> */}
+    </div>
+    </BrowserRouter>
     </>
   )
 }
